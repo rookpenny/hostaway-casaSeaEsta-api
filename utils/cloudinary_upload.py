36831686c7abd1df77a9b91c65e89522c13b4670
rookpenny <corbett.jarrod@gmail.com>
2025-11-07 23:25:06 -0500

    Update cloudinary_upload.py

--- utils/cloudinary_upload.py
+++ utils/cloudinary_upload.py
@@ -1,1 +1,22 @@
-dsfasdf
+# utils/cloudinary_upload.py
+
+import cloudinary
+import cloudinary.uploader
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+cloudinary.config(
+    cloud_name=os.getenv("CLOUDINARY_CLOUD_NAME"),
+    api_key=os.getenv("CLOUDINARY_API_KEY"),
+    api_secret=os.getenv("CLOUDINARY_API_SECRET")
+)
+
+def upload_image_from_url(url, filename=None):
+    upload_options = {}
+    if filename:
+        upload_options["public_id"] = filename.split('.')[0]
+
+    result = cloudinary.uploader.upload(url, **upload_options)
+    return result["secure_url"]
