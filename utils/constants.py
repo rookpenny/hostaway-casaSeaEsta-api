e0513e19bcc55c8e5f7229f0e5c8d20aba311167
rookpenny <corbett.jarrod@gmail.com>
2025-11-13 23:19:45 -0500

    Create constants.py

---
+++ utils/constants.py
@@ -0,0 +1,4 @@
+# ----------- CONSTANTS -----------
+ALLOWED_LISTING_IDS = {"256853"}  # Expand this if adding more properties
+LEGACY_PROPERTY_MAP = {"casa-sea-esta": "256853"}  # Consider removing this when all configs move to file-based
+EMERGENCY_PHONE = "+1-650-313-3724"  # Consider moving this to per-property config
