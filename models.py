e78e1a086d4bdd9de13f9ced454c766f2359372d
rookpenny <corbett.jarrod@gmail.com>
2025-11-25 02:35:03 -0500

    Update models.py

--- models.py
+++ models.py
@@ -6,13 +6,12 @@ Base = declarative_base()
 class PMC(Base):
     __tablename__ = "pmc"
 
-    id = Column(Integer, primary_key=True, index=True)
     pmc_name = Column(String, nullable=False)
     email = Column(String, nullable=True)
     main_contact = Column(String, nullable=True)
     subscription_plan = Column(String, nullable=True)
     pms_integration = Column(String, nullable=True)
-    pms_client_id = Column(String, nullable=True)
+    pms_client_id = Column(String, pms_client_id, nullable=True)
     pms_secret = Column(String, nullable=True)
     pms_account_id = Column(Integer, unique=True, index=True)
     active = Column(Boolean, default=False)
