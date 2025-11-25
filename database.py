0b8c7f8c267e7fd929a023752e0224b34a160188
rookpenny <corbett.jarrod@gmail.com>
2025-11-25 01:16:50 -0500

    Update database.py

--- database.py
+++ database.py
@@ -5,7 +5,7 @@ import os
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-engine = create_engine(DATABASE_URL)
+engine = create_engine(DATABASE_URL, echo=True, future=True)
 SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
 
 Base = declarative_base()
