8fb636004faf2cbc7f81d48e25b3480c6e8f3a84
rookpenny <corbett.jarrod@gmail.com>
2025-11-25 00:26:38 -0500

    Update database.py

--- database.py
+++ database.py
@@ -1,9 +1,7 @@
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker, declarative_base
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 import os
-from dotenv import load_dotenv
-
-load_dotenv()  # loads .env variables
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
