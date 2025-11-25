ac7287c2fceae50e3a990f7980b6c1d2bab758d1
rookpenny <corbett.jarrod@gmail.com>
2025-11-25 01:15:11 -0500

    Update models.py

--- models.py
+++ models.py
@@ -1,15 +1,19 @@
-from sqlalchemy import Column, String, Boolean
-from database import Base
+from sqlalchemy import Column, Integer, String, Boolean
+from sqlalchemy.ext.declarative import declarative_base
+
+Base = declarative_base()
 
 class PMC(Base):
-    __tablename__ = "pmc"
+    __tablename__ = "pmcs"
 
-    id = Column(String, primary_key=True, index=True)
-    pmc_name = Column(String)
-    hostaway_account_id = Column(String)
-    main_contact = Column(String)
-    email = Column(String)
-    subscription_plan = Column(String)
-    pms_integration = Column(String)
-    active = Column(Boolean, default=True)
-    pms_account_id = Column(String)
+    id = Column(Integer, primary_key=True, index=True)
+    pmc_name = Column(String, nullable=False)
+    email = Column(String, nullable=True)
+    main_contact = Column(String, nullable=True)
+    subscription_plan = Column(String, nullable=True)
+    pms_integration = Column(String, nullable=True)
+    pms_client_id = Column(String, nullable=True)
+    pms_secret = Column(String, nullable=True)
+    pms_account_id = Column(Integer, unique=True, index=True)
+    active = Column(Boolean, default=False)
+    sync_enabled = Column(Boolean, default=False)
