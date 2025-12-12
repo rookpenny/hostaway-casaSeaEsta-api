ad8d939712c8f54de757a197cb6fce99b7d3a731
rookpenny <corbett.jarrod@gmail.com>
2025-12-12 07:25:38 -0500

    Update main.py

--- main.py
+++ main.py
@@ -62,13 +62,16 @@ app.include_router(seed_guides_router)
 # Middleware
 
 ALLOWED_ORIGINS = [
-    "https://hostaway-casaseaesta-api.onrender.com/",
+    "https://hostaway-casaseaesta-api.onrender.com",
 ]
 
 app.add_middleware(
     SessionMiddleware,
-    secret_key=os.getenv("SESSION_SECRET") or "fallbacksecret"
+    secret_key=os.getenv("SESSION_SECRET") or "fallbacksecret",
+    same_site="none",
+    https_only=True,
 )
+
 app.add_middleware(
     CORSMiddleware,
     allow_origins=ALLOWED_ORIGINS,
